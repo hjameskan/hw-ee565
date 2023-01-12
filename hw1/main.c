f16b2eecccafae8f052de33cd490ae7de183ac24
DarkKnight <darkknight@5Qz6aT6ZQ8H92e.local>
2023-01-11 20:26:26 -0800

    bug fix and added .gitignore

--- hw1/main.c
+++ hw1/main.c
@@ -15,8 +15,6 @@
 
 #define BACKLOG 10   // how many pending connections queue will hold
 
-// Function to handle the SIGCHLD signal
-// this is used to clean up any terminated child processes
 void sigchld_handler(int s) {
     // waitpid() might overwrite errno, so we save and restore it:
     int saved_errno = errno;
@@ -24,7 +22,6 @@ void sigchld_handler(int s) {
     errno = saved_errno;
 }
 
-// function to get the address of the client
 void *get_in_addr(struct sockaddr *sa) {
     if (sa->sa_family == AF_INET) {
         return &(((struct sockaddr_in*)sa)->sin_addr);
@@ -42,19 +39,17 @@ int main(void) {
     char s[INET6_ADDRSTRLEN];
     int rv;
 
-    // fill in the hints structure
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE; // use my IP
 
-    // get the address information of the server
     if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
         fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
         return 1;
     }
 
-    // loop through all the results and bind to the first we can
+     // loop through all the results and bind to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
@@ -66,21 +61,26 @@ int main(void) {
             perror("setsockopt");
             exit(1);
         }
-        if (bind(sockfd, p->ai_addr, p->ai_
+        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("server: bind");
+            continue;
+        }
+
+        break;
+    }
+
     if (p == NULL)  {
         fprintf(stderr, "server: failed to bind\n");
-        exit(1);
+        return 2;
     }
-    // all done with this structure
-    freeaddrinfo(servinfo); 
 
-    // listen for connections
+    freeaddrinfo(servinfo); // all done with this structure
+
     if (listen(sockfd, BACKLOG) == -1) {
         perror("listen");
         exit(1);
     }
-
-    // setup the sigchld handler
     sa.sa_handler = sigchld_handler; // reap all dead processes
     sigemptyset(&sa.sa_mask);
     sa.sa_flags = SA_RESTART;
@@ -88,33 +88,42 @@ int main(void) {
         perror("sigaction");
         exit(1);
     }
-
     printf("server: waiting for connections...\n");
 
-    // main accept() loop
-    while(1) { 
+    while(1) {  // main accept() loop
         sin_size = sizeof their_addr;
         new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        // check for errors
         if (new_fd == -1) {
             perror("accept");
             continue;
         }
-        // print the address of the client that connected
         inet_ntop(their_addr.ss_family,
             get_in_addr((struct sockaddr *)&their_addr),
             s, sizeof s);
         printf("server: got connection from %s\n", s);
-        // fork a new child process to handle the connection
+        // if (!fork()) { // this is the child process
+        //     close(sockfd); // child doesn't need the listener
+        //     if (send(new_fd, "Hello, world!", 13, 0) == -1)
+        //         perror("send");
+        //     close(new_fd);
+        //     exit(0);
+        // }
         if (!fork()) { // this is the child process
             close(sockfd); // child doesn't need the listener
-            if (send(new_fd, "Hello, world!", 13, 0) == -1)
-                perror("send");
+            char buffer[1024];
+            int numbytes;
+            numbytes = recv(new_fd, buffer, sizeof buffer, 0);
+            buffer[numbytes] = '\0';
+            if(strstr(buffer, "GET / HTTP/1.1")) {
+                char* message = "HTTP/1.1 200 OK\r\n"
+                                "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+                                "<html><body>Hello World!</body></html>\r\n";
+                send(new_fd, message, strlen(message), 0);
+            }
             close(new_fd);
             exit(0);
         }
-        // parent process doesn't need the new_fd
-        close(new_fd);  
+        close(new_fd);  // parent doesn't need this
     }
     return 0;
 }

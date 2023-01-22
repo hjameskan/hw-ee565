b116e24c47fa2a915bcdd64531e853fbb1f7ed7c
Winston Sun <92708794+W-Sun12@users.noreply.github.com>
2023-01-21 19:25:27 -0800

    Update main.c

--- hw1/src/main.c
+++ hw1/src/main.c
@@ -122,6 +122,22 @@ int main(int argc, char *argv[]) {
             s, sizeof s);
         printf("Server: got connection from %s\n", s);
 
+        // read the request
+        char buffer[1024] = {0};
+        int valread = read( new_fd , buffer, 1024);
+        if (valread < 0) {
+            perror("Error reading from socket");
+            close(new_fd);
+            continue;
+        }
+
+        // parse the request
+        char* req_method = strtok(buffer, " ");
+        char* req_path = strtok(NULL, " ");
+        char* req_protocol = strtok(NULL, "\r\n");
+
+
+
         if (!fork()) { // this is the child process
             close(sockfd); // child doesn't need the listener
             char buffer[1024];
@@ -259,11 +275,80 @@ int main(int argc, char *argv[]) {
                 close(fd);
             }
             else {
-                char* message = "HTTP/1.1 404 Not Found\r\n"
-                                "Content-Type: text/html; charset=UTF-8\r\n\r\n"
-                                "<html><body>404 Not Found</body></html>\r\n";
-                send(new_fd, message, strlen(message), 0);
-            }
+            //////////////////////////////////check file types added start//////////////////////////////////////////
+                char* ext = strrchr(req_path, '.');
+                char content_type[1024] = {0};
+                if (strcmp(ext, ".txt") == 0) {
+                    strcpy(content_type, "text/plain");
+                }
+                else if (strcmp(ext, ".css") == 0) {
+                    strcpy(content_type, "text/css");
+                }
+                else if (strcmp(ext, ".htm") == 0 || strcmp(ext, ".html") == 0) {
+                    strcpy(content_type, "text/html");
+                }
+                else if (strcmp(ext, ".gif") == 0) {
+                    strcpy(content_type, "image/gif");
+                }
+                else if (strcmp(ext, ".jpg") == 0 || strcmp(ext, ".jpeg") == 0) {
+                    strcpy(content_type, "image/jpeg");
+                }
+                else if (strcmp(ext, ".png") == 0) {
+                    strcpy(content_type, "image/png");
+                }
+                else if (strcmp(ext, ".js") == 0) {
+                    strcpy(content_type, "application/javascript");
+                }
+                else if (strcmp(ext, ".mp4") == 0 || strcmp(ext, ".webm") == 0 || strcmp(ext, ".ogg") == 0) {
+                    strcpy(content_type, "video/webm");
+                }
+                else {
+                    strcpy(content_type, "application/octet-stream");
+                }
+                char* file_path = req_path + 1;
+                if (access(file_path, F_OK) != -1) {
+                    int file_fd = open(file_path, O_RDONLY);
+                    if (file_fd == -1) {
+                        perror("Error opening file");
+                        close(new_fd);
+                        continue;
+                    }
+
+                    struct stat st;
+                    fstat(file_fd, &st);
+                    int file_size = st.st_size;
+
+                    char response[1024] = {0};
+                    sprintf(response, "HTTP/1.1 200 OK\r\nContent-Length: %d\r\nContent-Type: %s\r\n\r\n", file_size, content_type);
+                    if (write(new_fd, response, strlen(response)) < 0) {
+                        perror("Error writing to socket");
+                        close(new_fd);
+                        continue;
+                    }
+
+                    int bytes_sent = 0;
+                    while (bytes_sent < file_size) {
+                        int bytes_read = read(file_fd, buffer, sizeof(buffer));
+                        if (bytes_read <= 0)
+                            break;
+                        }
+                        if (write(new_fd, buffer, bytes_read) < 0) {
+                            perror("Error writing to socket");
+                            break;
+                        }
+                        bytes_sent += bytes_read;
+                    }
+
+                    close(file_fd);
+                }
+                else {
+             //////////////////////////////////check file types end//////////////////////////////////////////
+                    char response[1024] = {0};
+                    sprintf(response, "HTTP/1.1 404 Not Found\r\n\r\n");
+                    if (write(new_fd, response, strlen(response)) < 0) {
+                        perror("Error writing to socket");
+                    }
+                }
             close(new_fd);
             exit(0);
         }

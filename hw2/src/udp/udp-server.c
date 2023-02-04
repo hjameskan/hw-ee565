6bb3e43bed8fb06f32211b824815092c4c292744
DarkKnight <darkknight@5Qz6aT6ZQ8H92e.local>
2023-02-03 19:02:50 -0800

    working file transfer in packets

--- hw2/src/udp/udp-server.c
+++ hw2/src/udp/udp-server.c
@@ -1,110 +1,177 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <time.h>
 
-#define MAX_PACKET_SIZE 512
-#define MAX_RETRIES 5
-#define RTO_TIMEOUT 1000 // in milliseconds
+#define MAX_CONNECTIONS 10
+#define MAX_SEQ_NO 30
+#define WINDOW_SIZE 10
+#define MAX_DATA_LEN 20
+#define PACKET_DATA_SIZE 1024
+#define HEADER_SIZE 12
 
-// Define the structure for a packet
-struct Packet {
-    int seq_num;
-    int ack_num;
-    char data[MAX_PACKET_SIZE];
-};
+void *connection_handler(void *socket_desc);
+void transfer_file(int sockfd, struct sockaddr_in client_addr);
+int min(int a, int b);
 
-int send_packet(int sock, struct sockaddr_in *dest_addr, struct Packet *pkt)
-{
-    int bytes_sent = sendto(sock, (char *)pkt, sizeof(struct Packet), 0,
-                            (struct sockaddr *)dest_addr, sizeof(*dest_addr));
 
-    if (bytes_sent < 0) {
-        perror("sendto");
-        return -1;
+
+int main(int argc, char **argv){
+  if (argc != 2){
+    printf("Usage: %s <port>\n", argv[0]);
+    exit(0);
+  }
+  
+  printf("UDP server with PID %d\n", getpid());
+  fflush(stdout);
+
+  int port = atoi(argv[1]);
+
+  int sockfd;
+  struct sockaddr_in server_addr, client_addr;
+  socklen_t addr_size;
+  int n;
+
+  sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd < 0){
+    perror("[-]socket error");
+    exit(1);
+  }
+
+  memset(&server_addr, '\0', sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  n = bind(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr));
+  if (n < 0) {
+    perror("[-]bind error");
+    exit(1);
+  }
+
+  pthread_t thread_id[MAX_CONNECTIONS];
+  int i = 0;
+
+  while(1){
+    addr_size = sizeof(client_addr);
+    int *new_sock = malloc(sizeof(int));
+    *new_sock = sockfd;
+
+    if(pthread_create(&thread_id[i], NULL, connection_handler, (void*)new_sock) < 0){
+      perror("[-]could not create thread");
+      exit(1);
     }
 
-    return 0;
+    if(i >= MAX_CONNECTIONS - 1){
+      i = 0;
+      for(int j = 0; j < MAX_CONNECTIONS; j++){
+        pthread_join(thread_id[j], NULL);
+      }
+    } else {
+      i++;
+    }
+  }
+
+  return 0;
 }
 
-int receive_ack(int sock, struct sockaddr_in *src_addr, int seq_num)
-{
-    int ack_num;
-    socklen_t src_len = sizeof(*src_addr);
+void *connection_handler(void *socket_desc){
+  int sockfd = *(int*)socket_desc;
+  char buffer[1024];
+  struct sockaddr_in client_addr;
+  socklen_t addr_size;
 
-    int bytes_received = recvfrom(sock, &ack_num, sizeof(int), 0,
-                                  (struct sockaddr *)src_addr, &src_len);
+  addr_size = sizeof(client_addr);
+  bzero(buffer, 1024);
+  recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*)&client_addr, &addr_size);
+  if (strncmp(buffer, "transfer_file", strlen("transfer_file")) == 0) {
+    transfer_file(sockfd, client_addr);
+  }
+  // else {
+  //   printf("[+]Data recv from %s: %s\n", inet_ntoa(client_addr.sin_addr), buffer);
+  //   fflush(stdout);
+  //   sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&client_addr, addr_size);
+  //   printf("[+]Data sent to %s: %s\n", inet_ntoa(client_addr.sin_addr), buffer);
+  //   fflush(stdout);
+  // }
 
-    if (bytes_received < 0) {
-        perror("recvfrom");
-        return -1;
-    }
-
-    if (ack_num == seq_num) {
-        return 0;
-    }
-
-    return -1;
+  free(socket_desc);
+  return 0;
 }
 
-int main(int argc, char *argv[])
-{
-    int sock, retries, i, num_packets;
-    struct sockaddr_in dest_addr, src_addr;
+typedef struct {
+    int packet_number;
+    int ack;
+    int packet_data_size;
+    char packet_data[PACKET_DATA_SIZE];
+} Packet;
 
-    // Create a UDP socket
-    sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) {
-        perror("socket");
-        return -1;
+void transfer_file(int sockfd, struct sockaddr_in client_addr){
+    char buffer[1024];
+    char file_path[1024];
+    socklen_t addr_size;
+    addr_size = sizeof(client_addr);
+
+    bzero(file_path, 1024);
+    recvfrom(sockfd, file_path, 1024, 0, (struct sockaddr*)&client_addr, &addr_size);
+
+    printf("[+]Requested file path from %s: %s\n", inet_ntoa(client_addr.sin_addr), file_path);
+    fflush(stdout);
+
+    FILE *requested_file = fopen(file_path, "rb");
+    if(requested_file == NULL){
+        strcpy(buffer, "File not found\n");
+        sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&client_addr, addr_size);
+        printf("[-]File not found\n");
+        fflush(stdout);
+        return;
     }
 
-    // Set the destination address and port
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_port = htons(atoi(argv[2]));
-    inet_aton(argv[1], &dest_addr.sin_addr);
+    Packet packet;
+    int packet_number = 1;
+    int n_bytes = 0;
+    while((n_bytes = fread(packet.packet_data, 1, PACKET_DATA_SIZE, requested_file)) > 0){
+        packet.packet_number = packet_number;
+        packet.packet_data_size = n_bytes;
+        int size = sizeof(packet);
+        printf("[+]Sending packet %d of size %d\n", packet_number, n_bytes);
 
-    // Number of packets to send
-    num_packets = 10;
-
-    // Send multiple packets
-    for (i = 0; i < num_packets; i++) {
-        // Initialize the packet
-        struct Packet pkt;
-        memset(&pkt, 0, sizeof(pkt));
-        pkt.seq_num = i;
-
-        // Fill in the data to be sent
-        sprintf(pkt.data, "This is packet %d", i);
-
-        // Send the packet and wait for the ACK
-        retries = 0;
-        while (retries < MAX_RETRIES) {
-            if (send_packet(sock, &dest_addr, &pkt) < 0) {
-                perror("send_packet");
-								return -1;
+        int bytes_sent = 0;
+        while(bytes_sent < HEADER_SIZE + n_bytes){
+            int sent = sendto(sockfd, (char*) &packet + bytes_sent, HEADER_SIZE + n_bytes - bytes_sent, 0, (struct sockaddr*)&client_addr, addr_size);
+            if (sent < 0){
+                printf("[-]Failed to send data\n");
+                fflush(stdout);
+                fclose(requested_file);
+                return;
             }
+            bytes_sent += sent;
 
-            // Wait for the ACK with RTO timeout
-            usleep(RTO_TIMEOUT * 1000);
-
-            if (receive_ack(sock, &src_addr, pkt.seq_num) == 0) {
-                break;
-            }
-
-            retries++;
+            struct timespec sleepTime;
+            sleepTime.tv_sec = 0;
+            sleepTime.tv_nsec = 1000000; // 1 milliseconds = 1,000,000 nanoseconds
+            nanosleep(&sleepTime, NULL);
         }
 
-        if (retries == MAX_RETRIES) {
-            printf("Failed to receive ACK for packet %d after %d retries\n", i, retries);
-            return -1;
-        }
-
-        // The packet was successfully sent and acknowledged
-        printf("Packet %d sent and acknowledged\n", i);
+        packet_number++;
+        printf("packet size: %d\n", sizeof(packet));
+        bzero((char*) &packet, sizeof(packet));
     }
 
-    return 0;
+    fclose(requested_file);
+    printf("[+]File transfer complete\n");
+    fflush(stdout);
 }
+
+        // bzero((char*) &packet, HEADER_SIZE + PACKET_SIZE);
+
+int min(int a, int b){
+    return a < b ? a : b;
+}
+
